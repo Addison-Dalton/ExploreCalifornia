b21b1afc4991beb4092395b9a41845d9effc301c
Addison Dalton <zarch14@gmail.com>
2019-01-10 12:54:14 -0500

    Adding exersize static files and exploring configuration setting.

--- ExploreCalifornia/Startup.cs
+++ ExploreCalifornia/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ExploreCalifornia
@@ -20,15 +21,30 @@ namespace ExploreCalifornia
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseExceptionHandler("/error.html");
+
+            var configuration = new ConfigurationBuilder()
+                                    .AddEnvironmentVariables()
+                                    .AddJsonFile(env.ContentRootPath + "/config.json")
+                                    .AddJsonFile(env.ContentRootPath + "/config.development.json", true)
+                                    .Build();
+
+            if (configuration.GetValue<bool>("FeatureToggles:EnableDeveloperExceptions"))
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.Use(async (context, next) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                if (context.Request.Path.Value.Contains("invalid"))
+                {
+                    throw new Exception("ERROR!");
+                }
+
+                await next();
             });
+
+            app.UseFileServer();
         }
     }
 }
